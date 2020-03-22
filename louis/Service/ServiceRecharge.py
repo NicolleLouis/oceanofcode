442ff3c6f86f4e8a0edc776ad16e179b1940f58e
NicolleLouis <louisxnicolle@gmail.com>
2020-03-22 18:05:07 +0100

    Charge SILENCE and SONAR when possible

--- louis/Service/ServiceRecharge.py
+++ louis/Service/ServiceRecharge.py
@@ -5,4 +5,10 @@ class ServiceRecharge:
 
     @staticmethod
     def chose_recharge(context_data):
-        return ServiceRecharge.TORPEDO
+        # Decision order: TORPEDO > SILENCE > SONAR
+        if (context_data.current_turn_torpedo_cooldown is None or
+                context_data.current_turn_torpedo_cooldown > 0):
+            return ServiceRecharge.TORPEDO
+        if context_data.current_turn_silence_cooldown > 0:
+            return ServiceRecharge.SILENCE
+        return ServiceRecharge.SONAR
