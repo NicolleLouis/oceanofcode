59b173453406f7a8dcc200c32c95d6d7c125fb94
NicolleLouis <louisxnicolle@gmail.com>
2020-03-22 11:05:59 +0100

    Use torpedo as sonar v1

--- louis/GameClass/ContextData.py
+++ louis/GameClass/ContextData.py
@@ -1,5 +1,10 @@
+from Service import ServiceUtils
+from louis.Service import ServiceOrder
+
+
 class ContextData(object):
     def __init__(self):
+        # Current turn game input
         self.current_turn_x = None
         self.current_turn_y = None
         self.current_turn_my_life = None
@@ -10,6 +15,7 @@ class ContextData(object):
         self.current_turn_sonar_result = None
         self.current_turn_opponent_orders = None
 
+        # Last turn game input
         self.last_turn_turn_x = None
         self.last_turn_turn_y = None
         self.last_turn_turn_my_life = None
@@ -19,9 +25,14 @@ class ContextData(object):
         self.last_turn_turn_silence_cooldown = None
         self.last_turn_turn_sonar_result = None
         self.last_turn_turn_opponent_orders = None
+
+        # Last turn game output
         self.last_turn_own_orders = None
 
-    def update_turn_data(self, turn_data):
+        # computed field
+        self.enemy_was_damaged = None
+
+    def update_turn_data(self, turn_data, enemy_ship):
         self.current_turn_x = turn_data["x"]
         self.current_turn_y = turn_data["y"]
         self.current_turn_my_life = turn_data["my_life"]
@@ -31,6 +42,8 @@ class ContextData(object):
         self.current_turn_silence_cooldown = turn_data["silence_cooldown"]
         self.current_turn_sonar_result = turn_data["sonar_result"]
         self.current_turn_opponent_orders = turn_data["opponent_orders"]
+
+        self.analyse_turn_data(enemy_ship)
 
     def update_end_of_turn_data(self, orders):
         self.last_turn_turn_x = self.current_turn_x
@@ -45,3 +58,37 @@ class ContextData(object):
 
         self.last_turn_own_orders = orders
 
+    def analyse_turn_data(self, enemy_ship):
+        self.compute_custom_fields()
+        self.analyse_custom_fields(enemy_ship)
+
+    def compute_enemy_was_damaged(self):
+        if self.current_turn_opp_life is not None and self.last_turn_turn_opp_life is not None:
+            self.enemy_was_damaged = not (
+                    self.current_turn_opp_life == self.last_turn_turn_opp_life
+            )
+        else:
+            self.enemy_was_damaged = None
+
+    def analyse_enemy_damage(self, enemy_ship):
+        # TODO: Case damage taken -> guess if it's for own torpedo or not
+        if self.enemy_was_damaged:
+            return
+        # Case first turn
+        if self.last_turn_own_orders is None:
+            return
+            # case damage taken but not because of our own torpedo (Surface or enemy torpedo)
+        if not ServiceOrder.get_attack_order(self.last_turn_own_orders):
+            return
+        attack_order = ServiceOrder.get_attack_order(self.last_turn_own_orders)
+        position_torpedo = ServiceOrder.get_position_from_attack_order(attack_order)
+        enemy_ship.enemy_board.update_board_torpedo_did_not_hit_in_position(
+            position_torpedo,
+            enemy_ship.delta_position
+        )
+
+    def compute_custom_fields(self):
+        self.compute_enemy_was_damaged()
+
+    def analyse_custom_fields(self, enemy_ship):
+        self.analyse_enemy_damage(enemy_ship)
--- louis/GameClass/Position.py
+++ louis/GameClass/Position.py
@@ -1,7 +1,16 @@
+from louis.Service import ServiceUtils
+
+
 class Position(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def invert_position(self):
+        return Position(
+            x=self.x * -1,
+            y=self.y * -1
+        )
 
     def get_distance(self, position):
         return abs(self.x - position.x) + abs(self.y - position.y)
@@ -21,8 +30,8 @@ class Position(object):
 
     def add_position(self, position):
         return Position(
-            x=self.x + position.x,
-            y=self.y + position.y
+            x=int(self.x) + int(position.x),
+            y=int(self.y) + int(position.y)
         )
 
     def __str__(self):
